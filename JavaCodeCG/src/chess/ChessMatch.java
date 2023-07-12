0aa6c7caf84a9f583181be3dd52f0a5684c3911e
Willian <willian.neibertz@gmail.com>
2023-07-12 10:39:01 -0300

    BoardExcepiron and defensive programing implemented

--- JavaCodeCG/src/chess/ChessMatch.java
+++ JavaCodeCG/src/chess/ChessMatch.java
@@ -13,9 +13,9 @@ public class ChessMatch {
     }
 
     public  ChessPiece[][] getPieces(){
-        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-        for(int i = 0; i< board.getRows();i++){
-            for(int j = 0; j< board.getColumns();j++){
+        ChessPiece[][] mat = new ChessPiece[board.getRow()][board.getColumn()];
+        for(int i = 0; i< board.getRow(); i++){
+            for(int j = 0; j< board.getColumn(); j++){
                 mat[i][j] = (ChessPiece) board.piece(i,j);
             }
         }
@@ -24,9 +24,9 @@ public class ChessMatch {
 
 
     private void initialSetup(){
-        board.placePiece(new Rook(board,Color.WHITE),new Position(2,1));
-        board.placePiece(new King(board,Color.BLACK),new Position(0,4));
+        board.placePiece(new Rook(board,Color.WHITE),new Position(2,4));
         board.placePiece(new King(board,Color.WHITE),new Position(7,4));
+        board.placePiece(new King(board,Color.WHITE),new Position(0,4));
     }
 
 
