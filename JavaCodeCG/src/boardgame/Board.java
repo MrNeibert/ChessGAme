9e5dd383e0cbd163438b5b9a42308494bd849e97
Willian <willian.neibertz@gmail.com>
2023-07-13 15:32:01 -0300

    Check Logic for the kings. Can't put yourself in check and players can't ignore it.

--- JavaCodeCG/src/boardgame/Board.java
+++ JavaCodeCG/src/boardgame/Board.java
@@ -48,12 +48,14 @@ public class Board {
     }
 
     public Piece removePiece(Position position){
+
         if(!positionExists(position)){
             throw  new BoardException("Position not on the board");
         }
         if (piece(position) == null){
             return null;
         }
+
         Piece aux =piece(position);
         aux.position = null;
         pieces[position.getRow()][position.getColumn()] = null;
